339e45e35ef8a32d2a984f113eecccbf53be1365
Michal Bartnicki <michalbartnicki7@gmail.com>
2020-02-07 02:20:00 +0100

    merge categories and drop unnecessary

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import pandas as pd
 
+
+def merge_categories():
+    df['category'] = df.loc[:,
+                     ['Kategoria 2', 'Kategoria 3', 'Kategoria 4', 'Kategoria 5', 'Kategoria 6', 'Kategoria 7',
+                      'Kategoria 8']].apply(
+        lambda x: '-'.join(x.dropna().astype(str)), axis=1
+
+    )
+
+
+# df = df.assign(
+#     category=df['Kategoria 2'].dropna().astype(str) + '-' + df['Kategoria 3'].astype(str) + '-' + df['Kategoria 4'].astype(
+#         str) + '-' + df['Kategoria 5'].astype(str) + '-' + df['Kategoria 6'].astype(str) + '-' + df[
+#                  'Kategoria 7'].astype(
+#         str) + '-' + df['Kategoria 8'].astype(str) + '-')
+
+
 df = pd.read_excel('test-data/antyki.xls', 'Transakcje')
 df = df.drop({'Lp', 'Data', 'Godzina', 'ID Sprzedawcy', 'Sprzedawca',
               'Miasto', 'Kod EAN', 'Do wyczer. zapas.',
@@ -21,11 +38,11 @@ print(df.columns)
 df.loc[df.is_new != 'nowy', 'is_new'] = 0
 df.loc[df.is_new == 'nowy', 'is_new'] = 1
 
-df['category'] = df.loc[:,
-                 ['Kategoria 2', 'Kategoria 3', 'Kategoria 4', 'Kategoria 5', 'Kategoria 6', 'Kategoria 7',
-                  'Kategoria 8']].apply(
-    lambda x: '-'.join(x.dropna().astype(str)), axis=1
+merge_categories()
 
-)
-print(df.head())
-print(df.category.head())
+df = df.drop({'Kategoria 2', 'Kategoria 3', 'Kategoria 4', 'Kategoria 5', 'Kategoria 6', 'Kategoria 7',
+              'Kategoria 8'
+              }, axis=1)
+
+print(df.head(10))
+print(df.columns)
