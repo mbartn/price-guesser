5a517329cdcb8ef5ae5b0db9a8851a4504df372a
Michal Bartnicki <michalbartnicki7@gmail.com>
2020-02-16 21:49:18 +0100

    train WIP - preparing data

--- bin/main.py
+++ bin/main.py
@@ -5,4 +5,5 @@ class Main:
     if __name__ == "__main__":
         price_guesser = PriceGuesser()
         # price_guesser.prepare_data()
-        price_guesser.describeData()
+        # price_guesser.describeData()
+        price_guesser.train()
--- bin/price_guesser.py
+++ bin/price_guesser.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 class PriceGuesser:
@@ -68,11 +69,62 @@ class PriceGuesser:
         plt.title('Price Distribution', fontsize=12)
         plt.show()
 
-    @staticmethod
-    def merge_categories(df):
-        df['category'] = df.loc[:,
-                         ['Kategoria 2', 'Kategoria 3', 'Kategoria 4', 'Kategoria 5', 'Kategoria 6', 'Kategoria 7',
-                          'Kategoria 8']].apply(
-            lambda x: '-'.join(x.dropna().astype(str)), axis=1
+        price_from_users = train.loc[df['is_shop'] == 'N', 'price']
+        price_from_shops = train.loc[df['is_shop'] == 'T', 'price']
 
-        )
+        fig, ax = plt.subplots(figsize=(18, 8))
+        ax.hist(np.log(price_from_shops + 1), color='#007D00', alpha=1.0, bins=50, range=[0, 10],
+                label='Price from shops')
+        ax.hist(np.log(price_from_users + 1), color='#8CB4E1', alpha=0.7, bins=50, range=[0, 10],
+                label='Price from users')
+        plt.xlabel('price', fontsize=12)
+        plt.ylabel('frequency', fontsize=12)
+        plt.title('Price Distribution by Shipping Type', fontsize=15)
+        plt.tick_params(labelsize=12)
+        plt.legend()
+        plt.show()
+
+        print('The average price is {}'.format(round(price_from_users.mean(), 2)), 'from users');
+        print('The average price is {}'.format(round(price_from_shops.mean(), 2)), 'from sellers')
+
+    def train(self):
+        df = pd.read_excel('test-data/antyki-prepared-data.xls', 'antyki')
+        print(df.columns)
+        to_categorical(df)
+        msk = np.random.rand(len(df)) < 0.8
+        train = df[msk]
+        test = df[~msk]
+        test_new = test.drop('price', axis=1)
+        y_test = np.log1p(test["price"])
+
+        nrow_train = train.shape[0]
+        y = np.log1p(train["price"])
+        merge: pd.DataFrame = pd.concat([train, test_new])
+
+        cv = CountVectorizer(min_df=10)
+        X_name = cv.fit_transform(merge['title'])
+        cv = CountVectorizer()
+        X_category = cv.fit_transform(merge['category'])
+
+
+
+def to_categorical(dataset):
+    dataset['auction_type'] = dataset['auction_type'].astype('category')
+    dataset['is_new'] = dataset['is_new'].astype('category')
+    dataset['is_shop'] = dataset['is_shop'].astype('category')
+    dataset['mark_zone'] = dataset['mark_zone'].astype('category')
+    dataset['wyroznienie_promotion'] = dataset['wyroznienie_promotion'].astype('category')
+    dataset['str_dzialu_promotion'] = dataset['str_dzialu_promotion'].astype('category')
+    dataset['pogrubienie_promotion'] = dataset['pogrubienie_promotion'].astype('category')
+    dataset['podswietlenie_promotion'] = dataset['podswietlenie_promotion'].astype('category')
+    dataset['category'] = dataset['category'].astype('category')
+
+
+@staticmethod
+def merge_categories(df):
+    df['category'] = df.loc[:,
+                     ['Kategoria 2', 'Kategoria 3', 'Kategoria 4', 'Kategoria 5', 'Kategoria 6', 'Kategoria 7',
+                      'Kategoria 8']].apply(
+        lambda x: '-'.join(x.dropna().astype(str)), axis=1
+
+    )
